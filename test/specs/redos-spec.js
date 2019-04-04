416dd6966c8109dca48094f5ff94169a58d65236
Anders Kaseorg <andersk@mit.edu>
2019-04-04 00:06:59 -0700

    redos specs: Add 1s timeout check

--- test/specs/redos-spec.js
+++ test/specs/redos-spec.js
@@ -10,7 +10,13 @@ describe('ReDOS tests', () => {
 
     it(file, () => {
       const spec = require(path.resolve(__dirname, '../redos', file));
+      const before = process.hrtime();
       expect(spec).toRender(spec.html);
+      const elapsed = process.hrtime(before);
+      if (elapsed[0] > 0) {
+        const s = (elapsed[0] + elapsed[1] * 1e-9).toFixed(3);
+        fail(`took too long: ${s}s`);
+      }
     });
   });
 });
